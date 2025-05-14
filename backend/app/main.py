85a0080d58485bc174f6df2997b10473741a0c7e
Shrish Jaiswal <shrishjais@gmail.com>
2025-05-14 19:50:14 -0400

    add fronend url for cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,12 @@
-from fastapi import FastAPI
-from app.services.car_identifier import CarIdentifierService
 from app.models.car import CarInfo, IdentifyCarInput
+from app.services.car_identifier import CarIdentifierService
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 service = CarIdentifierService()
 
-origins = [
-    "http://localhost:3000",
-]
+origins = ["http://localhost:3000", "https://carlens-theta.vercel.app/"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -17,11 +15,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.get("/")
 def root():
     return {
         "Hello": "Welcome to car-lens-pwa backend. Probably on the wrong endpoint I believe?"
     }
+
 
 @app.post("/get_car_info", response_model=CarInfo)
 def identify_car(params: IdentifyCarInput) -> CarInfo:
